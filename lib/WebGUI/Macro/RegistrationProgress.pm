3ae3e9ba0212bba52ca78454781e776e7ce3a5bb
Martin Kamerbeek <martin@martintwee.oqapi.nl>
2010-01-21 15:45:29 +0100

    Change RegistrationProcess macro built-in template to something that works without css.

--- lib/WebGUI/Macro/RegistrationProgress.pm
+++ lib/WebGUI/Macro/RegistrationProgress.pm
@@ -19,20 +19,43 @@ $session->errorHandler->warn(Dumper($status));
     my $var;
     $var->{ step_loop } = $status;
 
+#    my $template = <<EOT;
+#<tmpl_loop step_loop>
+#<div class="mainstep <tmpl_if isCurrentStep>currentStep</tmpl_if>">
+#    <div class="stepNumber"><tmpl_var stepNumber></div>
+#    <div class="stepName"><tmpl_var stepName></div>
+#    <div class="stepComplete <tmpl_if stepComplete>checked<tmpl_else>unchecked</tmpl_if>"></div>
+#    <tmpl_if substep_loop><tmpl_loop substep_loop>
+#    <div class="substep <tmpl_if substepComplete>checked<tmpl_else>unchecked</tmpl_if> <tmpl_if isCurrentSubstep>currentSubstep</tmpl_if>">
+#        <tmpl_var substepName>
+#    </div>
+#    </tmpl_loop></tmpl_if>
+#</div>
+#</tmpl_loop>
+#EOT
     my $template = <<EOT;
+<ul>
 <tmpl_loop step_loop>
-<div class="mainstep <tmpl_if isCurrentStep>currentStep</tmpl_if>">
-    <div class="stepNumber"><tmpl_var stepNumber></div>
-    <div class="stepName"><tmpl_var stepName></div>
-    <div class="stepComplete <tmpl_if stepComplete>checked<tmpl_else>unchecked</tmpl_if>"></div>
-    <tmpl_if substep_loop><tmpl_loop substep_loop>
-    <div class="substep <tmpl_if substepComplete>checked<tmpl_else>unchecked</tmpl_if> <tmpl_if isCurrentSubstep>currentSubstep</tmpl_if>">
-        <tmpl_var substepName>
-    </div>
-    </tmpl_loop></tmpl_if>
-</div>
+    <li> 
+        <tmpl_var stepNumber>. <tmpl_var stepName> 
+        <tmpl_if isCurrentStep>(current)</tmpl_if>
+        <tmpl_if stepComplete>(complete)</tmpl_if>
+        <tmpl_if substep_loop>
+            <ul>
+            <tmpl_loop substep_loop>
+                <li>
+                    <tmpl_var substepName>
+                    <tmpl_if substepComplete>(complete)</tmpl_if> 
+                    <tmpl_if isCurrentSubstep>(current)</tmpl_if>
+                </li>
+            </tmpl_loop>
+            </ul>
+        </tmpl_if>
+    </li>
 </tmpl_loop>
+</ul>
 EOT
+
     return WebGUI::Asset::Template->processRaw($session, $template, $var);
 }
 
