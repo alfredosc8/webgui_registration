483224f9086bc806a117c2cd0e9dbd108b6437b5
Martin Kamerbeek <martin@martintwee.oqapi.nl>
2010-03-18 18:05:49 +0100

    Adding basic CreateAccount step.

---
+++ installRegistration.pl
@@ -0,0 +1,114 @@
+#!/usr/bin/env perl
+
+$|++; # disable output buffering
+our ($webguiRoot, $configFile, $help, $man);
+
+BEGIN {
+    $webguiRoot = "..";
+    unshift (@INC, $webguiRoot."/lib");
+}
+
+use strict;
+use Pod::Usage;
+use Getopt::Long;
+use WebGUI::Session;
+use WebGUI::Registration::Step;
+
+GetOptions(
+    'configFile=s'  => \$configFile,
+);
+
+
+my $session = start( $webguiRoot, $configFile );
+
+installRegistrationStepTables( $session );
+
+finish( $session );
+
+#----------------------------------------------------------------------------
+sub installRegistrationStepTables {
+    my $session = shift;
+    print "Installiung registration step table...";
+
+    WebGUI::Registration::Step->crud_createTable( $session );
+
+    print "Done\n";
+}
+
+#----------------------------------------------------------------------------
+sub start {
+    my $webguiRoot  = shift;
+    my $configFile  = shift;
+    my $session = WebGUI::Session->open($webguiRoot,$configFile);
+    $session->user({userId=>3});
+    
+    ## If your script is adding or changing content you need these lines, otherwise leave them commented
+    #
+    # my $versionTag = WebGUI::VersionTag->getWorking($session);
+    # $versionTag->set({name => 'Name Your Tag'});
+    #
+    ##
+    
+    return $session;
+}
+
+#----------------------------------------------------------------------------
+sub finish {
+    my $session = shift;
+    
+    ## If your script is adding or changing content you need these lines, otherwise leave them commented
+    #
+    # my $versionTag = WebGUI::VersionTag->getWorking($session);
+    # $versionTag->commit;
+    ##
+    
+    $session->var->end;
+    $session->close;
+}
+
+__END__
+
+
+=head1 NAME
+
+utility - A template for WebGUI utility scripts
+
+=head1 SYNOPSIS
+
+ utility --configFile config.conf ...
+
+ utility --help
+
+=head1 DESCRIPTION
+
+This WebGUI utility script helps you...
+
+=head1 ARGUMENTS
+
+=head1 OPTIONS
+
+=over
+
+=item B<--configFile config.conf>
+
+The WebGUI config file to use. Only the file name needs to be specified,
+since it will be looked up inside WebGUI's configuration directory.
+This parameter is required.
+
+=item B<--help>
+
+Shows a short summary and usage
+
+=item B<--man>
+
+Shows this document
+
+=back
+
+=head1 AUTHOR
+
+Copyright 2001-2009 Plain Black Corporation.
+
+=cut
+
+#vim:ft=perl
---
+++ lib/WebGUI/Registration/Step/CreateAccount.pm
@@ -0,0 +1,155 @@
+package WebGUI::Registration::Step::CreateAccount;
+
+use strict;
+
+use Data::Dumper;
+use List::Util qw{ first };
+
+use base qw{ WebGUI::Registration::Step };
+
+#-------------------------------------------------------------------
+sub apply {
+    my $self    = shift;
+    my $session = $self->session;
+    my $user    = $self->registration->instance->user;
+
+}
+
+#-------------------------------------------------------------------
+sub crud_definition {
+    my $class       = shift;
+    my $session     = shift;
+    my $definition  = $class->SUPER::crud_definition( $session );
+
+    $definition->{ dynamic }->{ createAccountTemplateId } = {
+        fieldType   => 'template',
+        label       => 'Template',
+        tab         => 'properties',
+        namespace   => 'Registration/Step/CreateAccount',
+    };
+
+    return $definition;
+}
+
+##-------------------------------------------------------------------
+#sub getEditForm {
+#    my $self    = shift;
+#    my $session = $self->session;
+#    my $tabform = $self->SUPER::getEditForm();
+#
+#    return $tabform; 
+#}
+
+##-------------------------------------------------------------------
+#sub getSummaryTemplateVars {
+#    my $self            = shift;
+#    my $session         = $self->session;
+#    my $user            = $self->registration->instance->user;
+#    my @categoryLoop;
+#
+#    return @categoryLoop;
+#}
+
+#-------------------------------------------------------------------
+sub isComplete {
+    my $self = shift;
+
+    return !$self->session->user->isVisitor;
+}
+
+##-------------------------------------------------------------------
+#sub updateFromFormPost {
+#    my $self = shift;
+#
+#    $self->SUPER::updateFromFormPost;
+#
+#    $self->update({
+#        profileSteps        => $profileSteps,
+#        profileOverrides    => $profileOverrides, 
+#    });
+#}
+
+
+#-------------------------------------------------------------------
+sub processStepFormData {
+    my $self    = shift;
+    my $session = $self->session;
+    my $form    = $session->form;
+
+    my @required = qw{ username email identifier identifierConfirm };
+    foreach ( @required ) {
+        $self->pushError( "$_ is required" ) unless $form->get( $_ );
+    }
+
+    my $requestedUser   = WebGUI::User->newByUsername( $session, $form->get('username') );
+    my $emailUser       = WebGUI::User->newByEmail( $session, $form->get('email') );
+
+    if ( $requestedUser && $requestedUser->isEnabled ) {
+        $self->pushError( 'The requested username is already in use by another user' );
+    };
+    if ( $emailUser && $emailUser->isEnabled ) {
+        $self->pushError( 'The requested email address is already in use by another account.' );
+    };
+    if ( $form->get('identifier') ne $form->get('identifierConfirm') ) {
+        $self->pushError( 'The password you entered doesn\'t match its confirmation' );
+    };
+
+    unless ( @{$self->error} ) {
+        my $user = $self->registration->instance->user;
+
+        if ( !$user->isEnabled ) {
+            $user->username( $form->get('username') );
+            $user->update( { email => $form->get('email') } );
+            $user->disable;
+
+            my $auth = WebGUI::Auth::WebGUI->new( $session );
+            $auth->saveParams( $user->userId, $auth->authMethod, { 
+                identifier => $auth->hashPassword( $form->get('identifier') ) 
+            } );
+        }
+
+        if ($session->user->isVisitor) {
+            $session->user( { userId => $user->getId } );
+        }
+    }
+
+    # Return no errors since there aren't any.
+    return [];
+};
+
+#-------------------------------------------------------------------
+sub getViewVars {
+    my $self    = shift;
+    my $session = $self->session;
+    my $form    = $session->form;
+
+    my @fields;
+    push @fields, {
+        field_label         => 'Username',
+        field_formElement   => WebGUI::Form::text( $session, { name=>'username', value => $form->get('username') } ),
+        field_isRequired    => 1,
+    };
+    push @fields, {
+        field_label         => 'Password',
+        field_formElement   => WebGUI::Form::password( $session, { name=>'identifier', value => $form->get('identifier') } ),
+        field_isRequired    => 1,
+    };
+    push @fields, {
+        field_label         => 'Password confirmation',
+        field_formElement   => WebGUI::Form::password( $session, { name=>'identifierConfirm', value => $form->get('identifierConfirm') } ),
+        field_isRequired    => 1,
+    };
+    push @fields, {
+        field_label         => 'Password confirmation',
+        field_formElement   => WebGUI::Form::email( $session, { name=>'email', value => $form->get('email') } ),
+        field_isRequired    => 1,
+    };
+
+    my $var = $self->SUPER::getViewVars;
+    push @{ $var->{ field_loop } }, @fields;
+
+    return $var;
+}
+
+1;
+
