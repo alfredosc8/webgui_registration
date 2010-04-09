b05e4b41f7104e00d1f87075c60adefe406767fa
Martin Kamerbeek <martin@martintwee.oqapi.nl>
2010-04-09 17:01:43 +0200

    Forgot to add dutch trranslations fro create account.

---
+++ lib/WebGUI/i18n/Dutch/Step_CreateAccount.pm
@@ -0,0 +1,28 @@
+package WebGUI::i18n::Dutch::Step_CreateAccount;
+
+use strict;
+
+our %I18N = (
+    'is required' => {
+        message => 'is verplicht.',
+    },
+   
+    'username taken' => {
+        message => 'De gebruikersnaam is al in gebruik door een andere gebruiker.',
+    },
+
+    'pw doesnt match' => {
+        message => 'Het passwoord is niet gelijk aan de controle.',
+    },
+
+    'captcha wrong' => {
+        message => 'De captcha die u heeft ingevoerd is niet gelijk aan het plaatje.',
+    },
+
+    'account exists' => {
+        message => 'Er bestaat al een account met deze gebruikersnaam en/of emailadres. Klik <a href="%s">hier</a> als u uw paswoord of gebruikersnaam vergeten bent.',
+    },
+);
+
+1;
+
